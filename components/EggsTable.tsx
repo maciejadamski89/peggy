45da99b62ce8d95938fc4e1b0f325c7641686b85
Maciej Adamski <maciej.adamski@ocean.ibm.com>
2023-03-06 13:05:17 +0100

    feat: add new calculated dates

--- components/EggsTable.tsx
+++ components/EggsTable.tsx
@@ -8,8 +8,48 @@ export default function EggsTable({eggs}: {eggs: any}) {
 		let localDateTime = new Date(date);
 		const localDateTimeHours = localDateTime.getHours();
 		localDateTime.setHours(localDateTimeHours - localTimezoneOffsetHours);
-		return localDateTime.toISOString().replace("T", " ").slice(0, 16);
+		return localDateTime.toLocaleString("pl-PL", {
+			day: "2-digit",
+			month: "2-digit",
+			year: "numeric",
+			hour: "2-digit",
+			minute: "2-digit",
+		});
 	};
+
+	const generateHatchDate = (date: string, incubationDays: number) => {
+		const localTimezoneOffset = new Date().getTimezoneOffset();
+		const localTimezoneOffsetHours = localTimezoneOffset / 60;
+		let localDateTime = new Date(date);
+		const localDateTimeHours = localDateTime.getHours();
+		localDateTime.setHours(localDateTimeHours - localTimezoneOffsetHours);
+		localDateTime.setDate(localDateTime.getDate() + incubationDays);
+		return localDateTime.toLocaleString("pl-PL", {
+			day: "2-digit",
+			month: "2-digit",
+			year: "numeric",
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+	};
+
+	const generateTryShutdownDate = (date: string, incubationDays: number) => {
+		const daysBeforeHatch = 3;
+		const localTimezoneOffset = new Date().getTimezoneOffset();
+		const localTimezoneOffsetHours = localTimezoneOffset / 60;
+		let localDateTime = new Date(date);
+		const localDateTimeHours = localDateTime.getHours();
+		localDateTime.setHours(localDateTimeHours - localTimezoneOffsetHours);
+		localDateTime.setDate(localDateTime.getDate() + (incubationDays - daysBeforeHatch));
+		return localDateTime.toLocaleString("pl-PL", {
+			day: "2-digit",
+			month: "2-digit",
+			year: "numeric",
+			hour: "2-digit",
+			minute: "2-digit",
+		});
+	};
+
 	return (
 		<div className="flex flex-col">
 			<div className="-m-1.5 overflow-x-auto">
@@ -23,6 +63,12 @@ export default function EggsTable({eggs}: {eggs: any}) {
 										className="px-6 py-3 text-xs font-medium text-left text-gray-500 uppercase"
 									>
 										Nazwa jaja papugi
+									</th>
+									<th
+										scope="col"
+										className="px-6 py-3 text-xs font-medium text-left text-gray-500 uppercase"
+									>
+										Liczba dni inkubacji
 									</th>
 									<th
 										scope="col"
@@ -62,7 +108,16 @@ export default function EggsTable({eggs}: {eggs: any}) {
 											</Link>
 										</td>
 										<td className="px-5 py-4 text-sm font-medium text-gray-800 whitespace-nowrap ">
+											{egg.incubation_days}
+										</td>
+										<td className="px-5 py-4 text-sm font-medium text-gray-800 whitespace-nowrap ">
 											{convertDate(egg.created_at)}
+										</td>
+										<td className="px-5 py-4 text-sm font-medium text-gray-800 whitespace-nowrap ">
+											{generateTryShutdownDate(egg.created_at, egg.incubation_days)}
+										</td>
+										<td className="px-5 py-4 text-sm font-medium text-gray-800 whitespace-nowrap ">
+											{generateHatchDate(egg.created_at, egg.incubation_days)}
 										</td>
 										<td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
 											{/* <ButtonDeleteEgg eggId={egg.id} /> */}
